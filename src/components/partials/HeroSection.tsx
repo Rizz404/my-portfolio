4d3edba8356a5866ebf1bdd704f3eb62e679e7c0
Rizqiansyah Ramadhan <rizzthenotable@gmail.com>
2025-06-22 18:49:39 +0700

    fix(hero-section): remove aria label

--- src/components/partials/HeroSection.tsx
+++ src/components/partials/HeroSection.tsx
@@ -26,35 +26,30 @@ const HeroSection = ({ id }: HeroSectionProps) => {
         <div className="flex mt-6 space-x-4 text-lg text-primary">
           <a
             href="mailto:thenoblerizz@gmail.com"
-            aria-label={t("hero.emailAriaLabel")}
             className="hover:text-primary-hover"
           >
             <i className="fas fa-envelope"></i>
           </a>
           <a
             href="https://github.com/rizz404"
-            aria-label={t("hero.githubAriaLabel")}
             className="hover:text-primary-hover"
           >
             <i className="fab fa-github"></i>
           </a>
           <a
             href="https://www.linkedin.com/in/rizqiansyah-ramadhan-37b0b9313"
-            aria-label={t("hero.linkedinAriaLabel")}
             className="hover:text-primary-hover"
           >
             <i className="fab fa-linkedin-in"></i>
           </a>
           <a
             href="https://web.facebook.com/profile.php?id=61551041113414"
-            aria-label={t("hero.facebookAriaLabel")}
             className="hover:text-primary-hover"
           >
             <i className="fab fa-facebook-f"></i>
           </a>
           <a
             href="https://www.instagram.com/better_call_rizz"
-            aria-label={t("hero.instagramAriaLabel")}
             className="hover:text-primary-hover"
           >
             <i className="fab fa-instagram"></i>
